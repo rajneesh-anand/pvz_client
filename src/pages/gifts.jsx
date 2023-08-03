cd4756acbb4bba4a8a7b8d7898c5df5e636802a2
anand.k.rajneesh@gmail.com <anand.k.rajneesh@gmail.com>
2023-08-03 22:13:02 +0530

    gift page redirect

--- src/pages/gifts.jsx
+++ src/pages/gifts.jsx
@@ -1,6 +1,7 @@
 import Layout from "@components/layout";
 import axios from "axios";
 import GiftList from "@components/gift/gift-list";
+import { getSession } from "next-auth/react";
 
 export default function GiftPage({ giftList }) {
   return (
@@ -13,14 +14,24 @@ export default function GiftPage({ giftList }) {
 GiftPage.Layout = Layout;
 
 export const getServerSideProps = async (ctx) => {
-  const { page } = ctx.query;
-  const pageNumber = page ? page : 1;
+  const session = await getSession(ctx);
 
-  const { data } = await axios.get(
-    `${process.env.NEXT_PUBLIC_NODE_API_SERVER}/coin/redeem-list?limit=50&page=${pageNumber}`
-  );
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth/signin",
+        permanent: false,
+      },
+    };
+  } else {
+    const { page } = ctx.query;
+    const pageNumber = page ? page : 1;
+    const { data } = await axios.get(
+      `${process.env.NEXT_PUBLIC_NODE_API_SERVER}/coin/redeem-list?limit=50&page=${pageNumber}`
+    );
 
-  return {
-    props: { giftList: data.data },
-  };
+    return {
+      props: { giftList: data.data },
+    };
+  }
 };
